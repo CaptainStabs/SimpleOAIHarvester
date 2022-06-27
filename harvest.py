2ec946da0cd104997c49c4a115dd1d4973a60586
CaptainStabs <40151222+CaptainStabs@users.noreply.github.com>
2022-06-27 11:02:40 -0400

    fix indents

--- harvest.py
+++ harvest.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
+# Modified frmo https://github.com/Q42/SimpleOAIHarvester/blob/master/harvest.py
 import requests, os, sys
 from xml.dom import minidom
 
 if len(sys.argv) < 2: raise Exception('API key required')
 resumeFile = sys.argv[2] if len(sys.argv) >= 3 else None
 apikey = sys.argv[1]
+if len(sys.argv) == 3:
+    save_folder = sys.argv[3]
+else:
+    save_folder = ""
 
 url = "http://www.rijksmuseum.nl/api/oai/%s/?verb=listrecords&metadataPrefix=oai_dc" % apikey
 url2 = "http://www.rijksmuseum.nl/api/oai/%s/?verb=listrecords&resumptiontoken=" % apikey
@@ -12,51 +17,26 @@ count = 0 # keep track of number of records harvested
 token = ""
 
 def getText(nodelist):
-  rc = []
-  for node in nodelist:
-      if node.nodeType == node.TEXT_NODE:
-          rc.append(node.data)
-  return ''.join(rc)
+    rc = []
+    for node in nodelist:
+        if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+    return ''.join(rc)
 
 def harvest(url):
-  print("downloading: " + url)
-  data = requests.get(url).text
+    print("downloading: " + url)
+    data = requests.get(url).text
 
-  dom = minidom.parseString(data)
+    dom = minidom.parseString(data)
 
-  # check for error
-  error = dom.getElementsByTagName('error')
-  if len(error) > 0:
-    errType = error[0].getAttribute('code')
-    desc = getText(error[0].childNodes)
-    raise Exception(errType + ": " +desc)
+    # check for error
+    error = dom.getElementsByTagName('error')
+    if len(error) > 0:
+        errType = error[0].getAttribute('code')
+        desc = getText(error[0].childNodes)
+        raise Exception(errType + ": " +desc)
 
-  save(data)
-
-  countRecords = len(dom.getElementsByTagName('record'))
-
-  nodelist = dom.getElementsByTagName('resumptionToken')
-  if len(nodelist) == 0: return None, countRecords
-  strToken = getText(nodelist[0].childNodes)
-
-  return strToken, countRecords
-
-def save(data):
-  filename = str(count) + '.xml'
-  print('saving: ' + filename)
-  with open(filename, 'w', encoding='utf-8') as f:
-    for s in data:
-      f.write(s)
-
-def resume(filename):
-  with open(filename, 'r') as f:
-    data = f.read()
-     # cache the data because this file-like object is not seekable
-    cached  = ""
-    for s in data:
-      cached += s
-
-    dom = minidom.parseString(cached)
+    save(data)
 
     countRecords = len(dom.getElementsByTagName('record'))
 
@@ -66,24 +46,48 @@ def resume(filename):
 
     return strToken, countRecords
 
+def save(data):
+
+    filename = os.path.join(".", save_folder, str(count) + '.xml')
+    print('saving: ' + filename)
+    with open(filename, 'w') as f:
+        for s in data:
+            f.write(s)
+
+def resume(filename):
+    with open(filename, 'r') as f:
+        data = f.read()
+         # cache the data because this file-like object is not seekable
+        cached  = ""
+        for s in data:
+            cached += s
+
+        dom = minidom.parseString(cached)
+
+        countRecords = len(dom.getElementsByTagName('record'))
+
+        nodelist = dom.getElementsByTagName('resumptionToken')
+        if len(nodelist) == 0: return None, countRecords
+        strToken = getText(nodelist[0].childNodes)
+
+        return strToken, countRecords
+
 try:
+    if resumeFile:
+        token, countRecords = resume(resumeFile)
+        count += int(resumeFile.split('.')[0]) + countRecords
+    else:
+        token, countRecords = harvest(url)
+        count += countRecords
 
-  if resumeFile:
-      token, countRecords = resume(resumeFile)
-      count += int(resumeFile.split('.')[0]) + countRecords
-  else:
-    token, countRecords = harvest(url)
-    count += countRecords
-
-  while token:
-    token, countRecords = harvest(url2 + token)
-    count += countRecords
-
+    while token:
+        token, countRecords = harvest(url2 + token)
+        count += countRecords
 
 except:
-  print("\n!!!")
-  print("Unexpected error")
-  print("To resume run this script with the last succesfully harvested file as second paramater:")
-  print("python harvest.py <API KEY> <LAST HARVESTED FILE>")
-  print("!!!\n")
-  raise
+    print("\n!!!")
+    print("Unexpected error")
+    print("To resume run this script with the last succesfully harvested file as second paramater:")
+    print("python harvest.py <API KEY> <LAST HARVESTED FILE>")
+    print("!!!\n")
+    raise
